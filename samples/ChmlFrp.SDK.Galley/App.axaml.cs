52223cf88bde9aec017534b2b2a62ccd4091d720
Qusay <Qusay_Diaz.github@outlook.com>
2026-02-08 14:59:45 +0800

    更新ChmlFrp.SDK.Galley

--- samples/ChmlFrp.SDK.Galley/App.axaml.cs
+++ samples/ChmlFrp.SDK.Galley/App.axaml.cs
@@ -1,7 +1,5 @@
 using Avalonia;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using ChmlFrp.SDK.Galley.ViewModels;
 using ChmlFrp.SDK.Galley.Views;
 using ChmlFrp.SDK.Service;
 
@@ -10,20 +8,17 @@ namespace ChmlFrp.SDK.Galley;
 public class App : Application
 {
     public static readonly ChmlFrpClient ChmlFrpClient = new();
-    
+
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
-    public override async void OnFrameworkInitializationCompleted()
+    public override void OnFrameworkInitializationCompleted()
     {
-        foreach (var plugin in BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray())
-            BindingPlugins.DataValidators.Remove(plugin);
-
         var loginWindow = new LoginWindow();
         loginWindow.Show();
-        
-        var autoLoginAsync = await ChmlFrpClient.AutoLoginAsync();
+
+        /*var autoLoginAsync = await ChmlFrpClient.AutoLoginAsync();
         if (!autoLoginAsync!.State) return;
         new MainWindow { DataContext = new MainWindowViewModel(autoLoginAsync.Data!) }.Show();
-        loginWindow.Close();
+        loginWindow.Close();*/
     }
 }
